afde9293d90914922e94966ae97bced4f69640a4
Adrian Garcia <33211027+adgarciaar@users.noreply.github.com>
2018-02-12 19:08:47 -0500

    Add files via upload

--- Punto15.py
+++ Punto15.py
@@ -5,20 +5,35 @@ Created on Mon Feb 12 13:34:09 2018
 @author: adrian
 """
 
+import math
+from sympy import *
+from sympy.abc import x
+
+fIntegral = 5 - exp(u)
+fIzquierda = 2
+
+def f1(limiteInferior,limiteSuperior):
+    return integrate(fIntegral, (u, limiteInferior, limiteSuperior))
+
+def f2(x):
+    return fIzquierda    
+
+def fx(x):
+    limiteInferior = 0
+    limiteSuperior = x
+    return f1(0,x) - fIzquierda
+
+print("Función es ",fx(x))
+
+
 #GRÁFICA
 
-import math
 from matplotlib import pyplot
-# Función cuadrática.
-def f1(x):
-    return 5*x - math.exp(x)
-# Función lineal.
-def f2(x):
-    return 1
+
 # Valores del eje X que toma el gráfico.
 x = range(-10, 15)
 # Graficar ambas funciones.
-pyplot.plot(x, [f1(i) for i in x])
+pyplot.plot(x, [f1(0,i) for i in x])
 pyplot.plot(x, [f2(i) for i in x])
 # Establecer el color de los ejes.
 pyplot.axhline(0, color="black")
@@ -30,3 +45,26 @@ pyplot.ylim(-10, 10)
 #pyplot.savefig("output.png")
 # Mostrarlo.
 pyplot.show()
+
+
+#función equivalente
+
+# x = (1 + exp (x)) / 5 
+
+def g(x):
+    return (1 + exp(x)) / 5 
+
+#intervalo entre 0 y 3
+    
+def puntoFijo(x0, tolerancia, maxIt):
+    i = 1
+    while(i <= maxIt):
+        p = g(x0)
+        if(abs(p-x0) < tolerancia):
+            return p
+        i = i + 1
+        x0 = p
+    print("Método falló")
+
+print(puntoFijo(0.6,0.1,5))
+
