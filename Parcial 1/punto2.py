3768a9739b74fcc0a9d0681e82a236183c03777a
Adrian Garcia <33211027+adgarciaar@users.noreply.github.com>
2018-02-28 18:22:22 -0500

    punto 2

--- Parcial 1/punto2.py
+++ Parcial 1/punto2.py
@@ -21,30 +21,41 @@ def taylor(function,x0,n):
         i += 1
     return p
 
-def aitken(p0, tol, maxIt):
-    b = 0
-    i=1
-    while(i<=maxIt):
-        p1 = taylor(f,p0)
-        p2 = taylor(f,p1)
-        p = p0 - ((p1-p0)**2)/(p2-2*p1+p0)
-        
-        if(abs(p-p0)<tol):
-            print ("Solución aproximada es ",p)
-            b = 1
-            break
-        i += 1
-        p0 = p
-    
-    if(b == 0):
-        print("Método falló luego de ",maxIt)
-        
-func = str(taylor(f,0,5))
-print("Términos de la serie tomados: ",func)
+#n es número de términos y x es valor a evaluar
+def evaluarTaylor(n,valorEvaluacion): 
+    func = str(taylor(f,0,n))
+    x = valorEvaluacion
+    return eval(func)
 
-x = 1
-print("Evaluación cuando x = ",x," es ",eval(func))
-    
-#aitken(0.0, 0.00001, 1000)
+def aitken(x0,x1,x2,valorEvaluacion,E):        
+    err = 1
+    j = 3
+    i = 1
+    print("\t      Término \t     Iteración \t Error absoluto")
+    p0 = x2-((x2-x1)**2)/(x2-2*x1+x0) #fórmula
+    print("\t",p0,"\t                ",i)
+    while(err >= E):
+        p1 = p0        
+        x0 = evaluarTaylor(j-2,valorEvaluacion)
+        x1 = evaluarTaylor(j-1,valorEvaluacion)
+        x2 = evaluarTaylor(j,valorEvaluacion)        
+        p0 = x2-((x2-x1)**2)/(x2-2*x1+x0) #fórmula
+        err = (abs(p0-p1)/abs(p0))
+        j += 1
+        i += 1
+        print("\t",p0,"\t",i,"\t",err)
+
+#func = str(taylor(f,0,5))
+#print("Términos de la serie tomados: ",func)
+
+valorEvaluacion = 1
+
+x0 = evaluarTaylor(0,valorEvaluacion)
+x1 = evaluarTaylor(1,valorEvaluacion)
+x2 = evaluarTaylor(2,valorEvaluacion)
+
+E = 1.e-15
+
+aitken(x0, x1, x2, valorEvaluacion,E)
 
     
