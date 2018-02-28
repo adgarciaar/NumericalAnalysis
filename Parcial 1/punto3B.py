1f0ae0e390deba6af2be880634f623eb3a4d0cf2
Adrian Garcia <33211027+adgarciaar@users.noreply.github.com>
2018-02-28 18:33:27 -0500

    punto 3B

--- Parcial 1/punto3B.py
+++ Parcial 1/punto3B.py
@@ -7,21 +7,21 @@ gx="log(x+2)-sin(x)"
 g1x=str(diff(gx))
 g2x=str(diff(g1x))
 
-def main(x,tolerancia,N,i):
-
-    print('iteracion\tg(f(x))\t\terror\t\tderivada')
+def NewtonGeneralizado(x,tolerancia,N):
+    i = 1
+    print("Iteración \t\t g(f(x)) \t\t Error\n")
     while i<=N:
         d= float(eval(g1x))
         if d != 0:
             x0= x - (((eval(gx)*d))/((d**2)-eval(gx)*eval(g2x)))
-        er=abs(x0-x)
-        print("%d\t\t%.5f\t\t%.5f\t\t%.5f"%(i,x0,er,d));
-        if er<tolerancia:
-            print("La raiz es %.6f"%x0)            
+        error = (abs(x0-x) / abs(x0))
+        print(i,"\t\t",x0,"\t",error);
+        if error<tolerancia:
+            print("\nLa raiz es ",x0)            
             break
         i=i+1
         x=x0
     if i>=N:
         print("El método no converge ")
         
-main(-1.8, 0.00001,20,1)
+NewtonGeneralizado(-1.8, 1.e-5,20)
