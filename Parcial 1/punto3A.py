04e81f72f1dcb38743ce96289c2bebc7e6f9ee64
Adrian Garcia <33211027+adgarciaar@users.noreply.github.com>
2018-02-28 19:03:04 -0500

    punto 3

--- Parcial 1/punto3A.py
+++ Parcial 1/punto3A.py
@@ -10,12 +10,12 @@ import numpy as np
 def f(x): return np.log(x+2) - np.sin(x)
 def f1x(x): return (1/(x+2)) - np.cos(x)
 
-def secante(x0,x1):     
+def secante(x0,x1,E):     
   
   x = (f(x1)*x0-f(x0)*x1)/(f(x1)-f(x0))
   error = 1 
   
-  while (error > 1.e-7):      
+  while (error > E):      
     x0 = x1
     x1 = x
     x = (f(x1)*x0-f(x0)*x1)/(f(x1)-f(x0))
@@ -24,4 +24,4 @@ def secante(x0,x1):
     error = abs(f(x)/f1x(x))
     print("x = ",x,"\t","E = ",error)
   
-secante(-1.8,-1.0)
+secante(-1.8,-1.0,1.e-7)
