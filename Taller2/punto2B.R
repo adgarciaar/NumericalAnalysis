0f7d50e27d4794a0c44e66a7c8346feb3dfb23ea
Adrian Garcia <33211027+adgarciaar@users.noreply.github.com>
2018-03-09 12:02:01 -0500

    cambios

--- Taller2/punto2B.R
+++ Taller2/punto2B.R
@@ -1,8 +1,69 @@
-library(pracma)
-A = matrix(c(-8.1, -7.00, 6.123, -2.0, -1.0, 4.00,
-             -3.000, -1.0, 0.0, -1.00, -5.00, 0.6,
-             -1.0, 0.33, 6.000, 0.5), nrow=4, byrow=TRUE)
-A
-b <- c(1.45,3,5.12,???4)
-itersolve(A, b, tol = 1e-9, method = "Gauss-Seidel")
+itersolve <- function(A, b, x0 = NULL, 
+                      nmax = 1000, tol = .Machine$double.eps^(0.5),
+                      method = c("Gauss-Seidel", "Jacobi", "Richardson")) {
+  stopifnot(is.numeric(A), is.numeric(b))
+  
+  n <- nrow(A)
+  if (ncol(A) != n)
+    stop("Argument 'A' must be a square, positive definite matrix.")
+  b <- c(b)
+  if (length(b) != n)
+    stop("Argument 'b' must have the length 'n = ncol(A) = nrow(A).")
+  if (is.null(x0)) {
+    x0 <- rep(0, n)
+  } else {
+    stopifnot(is.numeric(x0))
+    x0 <- c(x0)
+    if (length(x0) != n)
+      stop("Argument 'x0' must have the length 'n=ncol(A)=nrow(A).")
+  }
+  
+  method <- match.arg(method)
+  
+  if (method == "Jacobi") {
+    L <- diag(diag(A))
+    U <- eye(n)
+    beta <- 1; alpha <- 1
+  } else if (method == "Gauss-Seidel") {
+    L <- tril(A)
+    U <- eye(n)
+    beta <- 1; alpha <- 1
+  } else {  # method = "Richardson"
+    L <- eye(n)
+    U <- L
+    beta <- 0
+  }
+  
+  b <- as.matrix(b)
+  x <- x0 <- as.matrix(x0)
+  r <- b - A %*% x0
+  r0 <- err <- norm(r, "f")
+  
+  iter <- 0
+  while (err > tol && iter < nmax) {
+    iter <- iter + 1
+    z <- qr.solve(L, r)
+    z <- qr.solve(U, z)
+    if (beta == 0) alpha <- drop(t(z) %*% r/(t(z) %*% A %*% z))
+    x <- x + alpha * z
+    r <- b - A %*% x
+    err <- norm(r, "f") / r0
+    cat("Iteración ",iter," -> Error relativo = ", err,"\n")
+  }
+  
+  cat ("\nNúmero de iteraciones realizado fue ",iter)
+  
+  cat ("\n\nSoluciones:")
+  print(c(x))
+}
 
+
+A = matrix(c(-8.1, -7, 6.123, -2, -1, 4,
+             -3, -1, 0, -1, -5, 0.6,
+             -1, 0.33, 6, 1/2), nrow=4, byrow=TRUE)
+
+b <- matrix(c(1.45,3,5.12,4.0), nrow = 4, ncol = 1, byrow = TRUE)
+
+cat("Mediante método de Gauss-Seidel\n")
+
+itersolve(A, b, tol = 1e-9, method = "Gauss-Seidel")
